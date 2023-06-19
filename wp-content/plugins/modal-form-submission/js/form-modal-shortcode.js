f719b5dc34e59482a1e791023397c337bee7353f
matthewpco <matthewpco86@gmail.com>
2023-06-19 01:04:12 +0000

    [~] Major refactor of code with new email and deletion options

--- wp-content/plugins/modal-form-submission/js/form-modal-shortcode.js
+++ wp-content/plugins/modal-form-submission/js/form-modal-shortcode.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let button = document.getElementById('custom-modal-form-button');
-    let overlay = document.getElementById('custom-modal-form-overlay');
-    let modal = document.getElementById('custom-modal-form-modal');
-    let form = document.getElementById('custom-modal-form');
+    let button = document.getElementById('form-modal-shortcode-button');
+    let overlay = document.getElementById('form-modal-shortcode-overlay');
+    let modal = document.getElementById('form-modal-shortcode-modal');
+    let form = document.getElementById('form-modal-shortcode');
     let close = document.getElementById('modal-close');
 
     button.addEventListener('click', function() {
@@ -23,32 +23,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-        overlay.style.display = 'none';
-        modal.style.display = 'none';
         let artist_name = document.getElementById('artist-name').value;
         let title_of_work = document.getElementById('title-of-work').value;
         let instagram_handle = document.getElementById('instagram-handle').value;
         let email = document.getElementById('email').value;
         let image_file = document.getElementById('image-file').value;
+        let success_message = document.getElementById('form-modal-shortcode-message');
+
 
         let xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 // Form submission successful
-                modal.style.display = 'none';
-                form.reset();
+                success_message.classList.toggle('hidden')
+                setTimeout(function() {
+                    overlay.style.display = 'none';
+                    modal.style.display = 'none';
+                    form.reset();
+                }, 3000);
+                
             }
         };
-        xhr.open('POST', custom_modal_form_ajax_object.ajax_url, true);
+        xhr.open('POST', form_modal_shortcode_ajax_object.ajax_url, true);
         xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         xhr.send(
-            'action=custom_modal_form_submit' +
+            'action=form_modal_shortcode_submit' +
             '&artist_name=' + artist_name +
             '&title_of_work=' + title_of_work +
             '&instagram_handle=' + instagram_handle +
             '&email=' + email +
             '&image_file=' + image_file +
-            '&_ajax_nonce=' + custom_modal_form_ajax_object.nonce
+            '&_ajax_nonce=' + form_modal_shortcode_ajax_object.nonce
         );
     });
 });
