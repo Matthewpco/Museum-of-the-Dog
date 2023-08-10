ae1ac8a469626ad98221140d4e307e08e20dca5f
matthewpco <matthewpco86@gmail.com>
2023-08-10 03:25:45 +0000

    [+] add nav menu items and styling

--- wp-content/themes/museumofthedog/template-parts/mobile-nav-menu.php
+++ wp-content/themes/museumofthedog/template-parts/mobile-nav-menu.php
@@ -8,48 +8,75 @@
 
     <div class="mobile-modal-content">
 
-        <div style="width: 90%">
-            <a href="/"><img src="/wp-content/uploads/boston-logo-white.webp" width="190px" alt="boston-logo-white" class="mobile-modal-logo"/></a>
+        <div style="width: 100%; display: flex; max-height: 100px">
+
+            <div style="width: 90%; padding-left: 5%; ">
+                <p><strong>Today's Hours:</strong> 11am - 6pm<i class="fa-solid fa-chevron-right" style="padding-left: 12px;"></i></p>
+                <a href="/plan-your-visit/directions-and-parking" style="color: #14377d;">Location<i class="fa-solid fa-location-dot" style="padding-left: 12px;"></i></a>
+            </div>
+
+            <div style="width: 10%">
+                <span class="close">&times;</span>
+            </div>
+
         </div>
 
-        <div style="width: 10%">
-            <span class="close">&times;</span>
+        
+
+        <div style="width: 100%; justify-content: space-evenly; color: white;" class="display-flex">
+            <a href="https://www.facebook.com/akcmuseumofthedog" style="color: white; font-size: 1.5rem;"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://www.instagram.com/museumofthedog" style="color: white; font-size: 1.5rem;"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://twitter.com/akcmod" style="color: white; font-size: 1.5rem;"><i class="fa-brands fa-twitter"></i></a>
+        </div>
+
+        <div style="width: 100%;">
+            <div style="background-color: #14377d; margin: 5%; text-align: center; padding: 1%;"><a href="/get-involved/donate" style=" color: white;">DONATE</a>
         </div>
 
         <div class="mobile-modal-content-menu">
             <nav>
                 <ul class="display-flex flex-direction-column">
-                    <li class="mobile-has-submenu">About<i class="fa-solid fa-chevron-down"></i>
-                    <ul class="mobile-submenu">
-                            <li><a href="/our-office">Our Office</a></li>
-                            <li><a href="/meet-dr-sabra">Meet Dr. Sabra</a></li>
+                    <li class="mobile-has-submenu">PLAN YOUR VISIT<i class="fa-solid fa-chevron-down"></i>
+                        <ul class="mobile-submenu">
+                            <li><a href="/">Our Office</a></li>
+                            <li><a href="/">Meet Dr. Sabra</a></li>
                         </ul>
                     </li>
-                    <li class="no-submenu"><a href="/periodontal-services">Periodontal Services</a></li>
-                    <li class="no-submenu"><a href="/dental-implants">Dental Implants</a></li>
-                    <li class="mobile-has-submenu">Patient Info<i class="fa-solid fa-chevron-down"></i>
-                    <ul class="mobile-submenu">
+                    <li class="no-submenu"><a href="/">BUY TICKETS</a></li>
+                    <li class="no-submenu"><a href="https://museumofthedogstore.org">MUSEUM STORE</a></li>
+                    <li class="mobile-has-submenu">ABOUT US<i class="fa-solid fa-chevron-down"></i>
+                        <ul class="mobile-submenu">
                             <li><a href="/first-visit">First Visit</a></li>
                             <li><a href="/post-op-care">Post-Op Care</a></li>
                         </ul>
                     </li>
-                    <li class="no-submenu"><a href="/referral-forms">Referral Forms</a></li>
+                    <li class="no-submenu"><a href="/">CONTACT</a></li>
+                    <li class="mobile-has-submenu">MUSEUM ATTRACTIONS<i class="fa-solid fa-chevron-down"></i>
+                        <ul class="mobile-submenu">
+                            <li><a href="/first-visit">First Visit</a></li>
+                            <li><a href="/post-op-care">Post-Op Care</a></li>
+                        </ul>
+                    </li>
+                    <li class="mobile-has-submenu">EVENTS & EDUCATION<i class="fa-solid fa-chevron-down"></i>
+                        <ul class="mobile-submenu">
+                            <li><a href="/first-visit">First Visit</a></li>
+                            <li><a href="/post-op-care">Post-Op Care</a></li>
+                        </ul>
+                    </li>
+                    <li class="mobile-has-submenu">MEMBERSHIP<i class="fa-solid fa-chevron-down"></i>
+                        <ul class="mobile-submenu">
+                            <li><a href="/first-visit">First Visit</a></li>
+                            <li><a href="/post-op-care">Post-Op Care</a></li>
+                        </ul>
+                    </li>
+                    <li class="mobile-has-submenu">GET INVOLVED<i class="fa-solid fa-chevron-down"></i>
+                        <ul class="mobile-submenu">
+                            <li><a href="/first-visit">First Visit</a></li>
+                            <li><a href="/post-op-care">Post-Op Care</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
-        </div>
-
-        <div class="mobile-modal-content-footer">
-          <address class="font-book">
-            <i class="fa-solid fa-location-dot"></i><a href="/contact">398 Commonwealth Ave, Boston, MA 02215</a>
-          </address>
-          <br>
-          <a href="tel: (617) 456-9714"><i class="fa-solid fa-phone"></i>(617) 456-9714</a>
-				  <br>
-          <a href="#" style="font-family: 'AvenirLTProBook';"><i class="fa-solid fa-clipboard"></i>Patient Portal / Forms</a>
-        </div>
-
-        <div class="mobile-modal-content-button">
-            <button class="">REQUEST CONSULTATION</button>
         </div>
 
     </div>
