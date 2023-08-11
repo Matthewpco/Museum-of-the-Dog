d751a636744c49aa57b35d392d1dcbfc944bfdd5
matthewpco <matthewpco86@gmail.com>
2023-08-11 03:35:55 +0000

    [+] add menu items

--- wp-content/themes/museumofthedog/template-parts/mobile-nav-menu.php
+++ wp-content/themes/museumofthedog/template-parts/mobile-nav-menu.php
@@ -10,9 +10,9 @@
 
         <div style="width: 100%; display: flex; max-height: 100px">
 
-            <div style="width: 90%; padding-left: 5%; ">
+            <div style="width: 90%; padding-left: 10%; color: white;">
                 <p><strong>Today's Hours:</strong> 11am - 6pm<i class="fa-solid fa-chevron-right" style="padding-left: 12px;"></i></p>
-                <a href="/plan-your-visit/directions-and-parking" style="color: #14377d;">Location<i class="fa-solid fa-location-dot" style="padding-left: 12px;"></i></a>
+                <a href="/plan-your-visit/directions-and-parking" class="white">Location<i class="fa-solid fa-location-dot" style="padding-left: 12px;"></i></a>
             </div>
 
             <div style="width: 10%">
@@ -30,29 +30,38 @@
         </div>
 
         <div style="width: 100%;">
-            <div style="background-color: #14377d; margin: 5%; text-align: center; padding: 1%;"><a href="/get-involved/donate" style=" color: white;">DONATE</a>
+            <div style="background-color: #14377d; margin: 10%; text-align: center; padding: 3%; border-radius: 3px;"><a href="/get-involved/donate" style=" color: white;">DONATE</a>
         </div>
 
         <div class="mobile-modal-content-menu">
             <nav>
-                <ul class="display-flex flex-direction-column">
-                    <li class="mobile-has-submenu">PLAN YOUR VISIT<i class="fa-solid fa-chevron-down"></i>
+                <ul class="display-flex flex-direction-column white">
+                    <li class="mobile-has-submenu white"><a href="/plan-your-visit">PLAN YOUR VISIT</a><i class="fa-solid fa-chevron-down"></i>
                         <ul class="mobile-submenu">
-                            <li><a href="/">Our Office</a></li>
-                            <li><a href="/">Meet Dr. Sabra</a></li>
+                            <li><a href="/plan-your-visit/our-neighborhood/">Our Neighborhood</a></li>
+                            <li><a href="/plan-your-visit/directions-and-parking/">directions & parking</a></li>
+                            <li><a href="/plan-your-visit/museum-map/">museum map</a></li>
+                            <li><a href="/plan-your-visit/facility-rental/">facility rental</a></li>
+                            <li><a href="/plan-your-visit/tours-and-group-visits/">groups and tours</a></li>
                         </ul>
                     </li>
                     <li class="no-submenu"><a href="/">BUY TICKETS</a></li>
                     <li class="no-submenu"><a href="https://museumofthedogstore.org">MUSEUM STORE</a></li>
-                    <li class="mobile-has-submenu">ABOUT US<i class="fa-solid fa-chevron-down"></i>
+                    <li class="mobile-has-submenu"><a href="/about-us">ABOUT US</a><i class="fa-solid fa-chevron-down"></i>
                         <ul class="mobile-submenu">
-                            <li><a href="/first-visit">First Visit</a></li>
-                            <li><a href="/post-op-care">Post-Op Care</a></li>
+                            <li><a href="/about/key-staff">key staff</a></li>
+                            <li><a href="/about/careers">Careers</a></li>
+                            <li><a href="/about/news">news</a></li>
                         </ul>
                     </li>
                     <li class="no-submenu"><a href="/">CONTACT</a></li>
-                    <li class="mobile-has-submenu">MUSEUM ATTRACTIONS<i class="fa-solid fa-chevron-down"></i>
+                    <li class="mobile-has-submenu"><a href="/museum-attractions">MUSEUM ATTRACTIONS</a><i class="fa-solid fa-chevron-down"></i>
                         <ul class="mobile-submenu">
+                            <li><a href="/plan-your-visit/our-neighborhood/">Our Neighborhood</a></li>
+                            <li><a href="/plan-your-visit/directions-and-parking/">directions & parking</a></li>
+                            <li><a href="/plan-your-visit/museum-map/">museum map</a></li>
+                            <li><a href="/plan-your-visit/facility-rental/">facility rental</a></li>
+                            <li><a href="/plan-your-visit/tours-and-group-visits/">groups and tours</a></li>
                             <li><a href="/first-visit">First Visit</a></li>
                             <li><a href="/post-op-care">Post-Op Care</a></li>
                         </ul>
